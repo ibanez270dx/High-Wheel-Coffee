72b6856b27231375795085430efb4a567cdfe78b
Jeff Miller <jeff@coverhound.com>
2012-08-16 18:25:11 -0700

    Initial commit, new Rails 3.2.8 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HighWheelCoffee::Application.config.secret_token = 'b427927802e2fa3d1b71d6a6496a08ff468bd8681f17a11b7e91f1576c6d4b10c2397e48a624b782f74fefaf3c625d1c6cd6c49cccc867f1546b766cc9d187c0'
